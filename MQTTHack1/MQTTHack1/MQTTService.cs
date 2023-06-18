6e688106ee50350f74c813bb2931c86214897f3d
Achim Friedland <achim.friedland@graphdefined.com>
2023-06-18 17:07:08 +0200

    Little test software

---
+++ MQTTHack1/MQTTHack1/MQTTService.cs
@@ -0,0 +1,57 @@
+﻿using MQTTnet;
+using MQTTnet.Client;
+using Newtonsoft.Json.Linq;
+using System.Security.Authentication;
+using System.Text;
+
+public class MqttClientService
+{
+
+    private IMqttClient?       client;
+    private MqttClientOptions? options;
+
+    public async Task ConnectAsync(String  hostname,
+                                   Int16   port,
+                                   String  username,
+                                   String  password,
+                                   String  topic)
+    {
+
+        var factory = new MqttFactory();
+        client = factory.CreateMqttClient();
+
+        // Optionen für die MQTT-Verbindung
+        options = new MqttClientOptionsBuilder().
+                      WithTcpServer   (hostname, port).
+                      WithCredentials (username, password).
+                      WithClientId    (Guid.NewGuid().ToString()).
+                      WithCleanSession().
+                      WithTls         (o => {
+                                                o.CertificateValidationHandler = _ => true;
+                                                o.SslProtocol = SslProtocols.Tls12;
+                                            }).
+                      Build           ();
+
+        // Handle event when mqtt messages are received.
+        client.ApplicationMessageReceivedAsync += async e => {
+
+            Console.WriteLine($"Topic: {e.ApplicationMessage.Topic}");
+            Console.WriteLine($"Payload: {Encoding.UTF8.GetString(e.ApplicationMessage.PayloadSegment)}\n");
+
+            var json = JObject.Parse(Encoding.UTF8.GetString(e.ApplicationMessage.PayloadSegment));
+
+            File.AppendAllText("hackathonJena4.log", e.ApplicationMessage.Topic);
+            File.AppendAllText("hackathonJena4.log", json.ToString() + Environment.NewLine + Environment.NewLine);
+
+        };
+
+        await client.ConnectAsync(options);
+
+        await client.SubscribeAsync(new MqttTopicFilterBuilder().
+                                        WithTopic(topic).
+                                        Build    ());
+
+    }
+
+}
+
---
+++ MQTTHack1/MQTTHack1/Program.cs
@@ -0,0 +1,14 @@
+﻿
+var mqttService = new MqttClientService();
+
+// wss://mqttbr.jena.de:8081
+// mqtt://mqttbr.jena.de:8883
+await mqttService.ConnectAsync(hostname:  "mqttbr.jena.de",
+                               port:       8883,
+                               username:  "login",
+                               password:  "password",
+                               topic:     "#");
+
+while (true) {
+    Thread.Sleep(1000);
+}
